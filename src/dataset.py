ae6a6461db38325fd0562fe0235922c8c7912e39
Pedro Lucas M Souza <73369350+pedrolucasms2@users.noreply.github.com>
2025-09-30 12:15:18 -0300

    get classes adicionado

--- src/dataset.py
+++ src/dataset.py
@@ -3,80 +3,80 @@ import torch
 import numpy as np
 from PIL import Image
 from pycocotools.coco import COCO
+from torch.utils.data import Dataset
+import torchvision.transforms.v2 as T
 
-class PestDetectionDataset(torch.utils.data.Dataset):
+class PestDetectionDataset(Dataset):
     def __init__(self, root_dir, annotation_file, transforms=None):
         """
         Args:
             root_dir (string): Diretório com todas as imagens.
             annotation_file (string): Caminho para o arquivo de anotação COCO.
-            transforms (callable, optional): Transformações a serem aplicadas na imagem e anotações.
+            transforms (callable, optional): Transformações opcionais a serem aplicadas
+                                             em uma amostra.
         """
-        self.root = root_dir
+        self.root_dir = root_dir
         self.coco = COCO(annotation_file)
         self.ids = list(sorted(self.coco.imgs.keys()))
         self.transforms = transforms
 
     def __getitem__(self, index):
-        """
-        Retorna uma amostra do dataset.
-        """
-        # Pega o ID da imagem
+        # Obtém o ID da imagem
         coco = self.coco
         img_id = self.ids[index]
         ann_ids = coco.getAnnIds(imgIds=img_id)
         coco_anns = coco.loadAnns(ann_ids)
-        
-        # Pega o caminho e carrega a imagem
+
+        # Caminho para a imagem
         path = coco.loadImgs(img_id)[0]['file_name']
-        img_path = os.path.join(self.root, path)
-        img = Image.open(img_path).convert("RGB")
+        img_path = os.path.join(self.root_dir, path)
+        img = Image.open(img_path).convert('RGB')
 
-        # Pega as bounding boxes
+        # Extrai as bounding boxes
         boxes = []
-        if coco_anns: # Garante que há anotações
-            for ann in coco_anns:
-                # O formato COCO é [x_min, y_min, width, height]
-                # O formato do PyTorch é [x_min, y_min, x_max, y_max]
-                x_min, y_min, width, height = ann['bbox']
-                x_max = x_min + width
-                y_max = y_min + height
-                boxes.append([x_min, y_min, x_max, y_max])
-        
-        # Evita criar um tensor vazio se não houver anotações
-        if boxes:
-            boxes = torch.as_tensor(boxes, dtype=torch.float32)
-        else:
-            boxes = torch.zeros((0, 4), dtype=torch.float32)
+        for ann in coco_anns:
+            # O formato COCO é [x_min, y_min, width, height]
+            # O formato do PyTorch é [x_min, y_min, x_max, y_max]
+            xmin = ann['bbox'][0]
+            ymin = ann['bbox'][1]
+            xmax = xmin + ann['bbox'][2]
+            ymax = ymin + ann['bbox'][3]
+            boxes.append([xmin, ymin, xmax, ymax])
 
-        # Pega os rótulos (neste caso, todos são da classe 1, "pest")
-        # Classe 0 é reservada para o fundo (background)
-        labels = torch.ones((len(coco_anns),), dtype=torch.int64)
-        
-        # Pega as máscaras de segmentação
+        boxes = torch.as_tensor(boxes, dtype=torch.float32)
+
+        # Extrai os labels (IDs das classes)
+        labels = torch.as_tensor([ann['category_id'] for ann in coco_anns], dtype=torch.int64)
+
+        # Extrai as máscaras de segmentação
         masks = []
-        if coco_anns and 'segmentation' in coco_anns[0]:
-            for ann in coco_anns:
-                mask = coco.annToMask(ann)
-                masks.append(mask)
+        for ann in coco_anns:
+            masks.append(coco.annToMask(ann))
         
+        # Converte a lista de máscaras em um único array numpy antes de criar o tensor
         if masks:
-            # Converte a lista de arrays numpy em um único array numpy antes de criar o tensor
-            masks = torch.as_tensor(np.array(masks), dtype=torch.uint8)
-        else:
-            # Cria um placeholder para máscaras se não houver anotações
-            h, w = img.size
-            masks = torch.zeros((0, w, h), dtype=torch.uint8)
+            masks = np.stack(masks, axis=0)
+            masks = torch.as_tensor(masks, dtype=torch.uint8)
+        else: # Caso não haja anotações na imagem
+            masks = torch.empty((0, img.height, img.width), dtype=torch.uint8)
 
-
-        # Cria o dicionário de anotações
+        # Monta o dicionário de 'targets'
         target = {}
         target["boxes"] = boxes
         target["labels"] = labels
         target["masks"] = masks
         target["image_id"] = torch.tensor([img_id])
         
-        # Aplica as transformações, se existirem
+        # Calcula a área das bounding boxes
+        if boxes.shape[0] > 0:
+            target["area"] = (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0])
+        else:
+            target["area"] = torch.empty((0,), dtype=torch.float32)
+
+        # Supõe que não há instâncias "crowd"
+        target["iscrowd"] = torch.zeros((len(coco_anns),), dtype=torch.int64)
+
+        # Aplica as transformações, se houver
         if self.transforms is not None:
             img, target = self.transforms(img, target)
 
@@ -84,4 +84,14 @@ class PestDetectionDataset(torch.utils.data.Dataset):
 
     def __len__(self):
         return len(self.ids)
+
+    def get_classes(self):
+        """
+        Retorna uma lista de nomes de classes do arquivo de anotação COCO.
+        """
+        if self.coco:
+            cats = self.coco.loadCats(self.coco.getCatIds())
+            # O len() disso nos dará o número de classes.
+            return cats
+        return []
 
